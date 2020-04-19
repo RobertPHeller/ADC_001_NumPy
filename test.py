56c8d7b8ff432e164dcffd9a0153c235aaf62539
Robert Heller <heller@deepsoft.com>
2020-04-19 19:05:27 -0400

    Minor fix to test.py (importing symbols to toplevel context).

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import sys
 sys.path.append("/usr/local/lib/python3.5/site-packages")
 import adc001py
-from adc001py import *
-adc001py.adc_config()
-adc001py.adc_set_samplerate(adc001py.SAMP_RATE_31250)
-adc001py.adc_set_chan0()
-volts = adc001py.adc_read_multiple(64)
+from adc001py.adc001py import *
+adc_config()
+adc_set_samplerate(SAMP_RATE_31250)
+adc_set_chan0()
+volts = adc_read_multiple(64)
 print(volts)
-adc001py.adc_quit()
+adc_quit()
 
